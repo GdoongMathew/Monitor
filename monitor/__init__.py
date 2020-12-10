8ca308c441a8ecc873523cef712f3b8097831723
GdoongMathew <dungdudu426@gmail.com>
2020-12-11 02:55:56 +0800

    1. code cleanup.

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -2,18 +2,4 @@ from .device_monitor import NVGPUMonitor
 from .device_monitor import CPUMonitor
 
 
-class Device:
-    device_id = None
-    device_name = ''
-
-    temperature = None
-    memory_total = None
-    memory_used = None
-    memory_free = None
-    memory_info = {}
-    clock_speed = None
-
-    _device_handle = None
-
-
 
